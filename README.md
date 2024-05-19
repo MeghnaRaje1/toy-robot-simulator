552f59e2b76699157f608eb4d494fc8d3872c601
Meghna Raje <meghna.raje1@gmail.com>
2024-05-19 11:53:32 +0100

    main code with README file

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# toy-robot-simulator
+# Toy Robot Simulator
+
+This project implements a Toy Robot Simulator in Python. The simulator allows a toy robot to move on a 5x5 square tabletop based on provided commands. The robot can be placed on the tabletop, moved, rotated left or right, and report its current position.
+
+## Tabletop Layout
+
+The tabletop has the following layout:
+
++---+---+---+---+---+
++---+---+---+---+---+
++---+---+---+---+---+
++---+---+---+---+---+
++---+---+---+---+---+
++---+---+---+---+---+
+
+Here, the south-west corner is (0,0).
+
+## Usage
+
+1. Clone the repository:
+2. Navigate to the directory: toy_robot_simulator
+3. Run the main.py file
+4. The simulator reads input commands from data/input.txt and writes output to data/output.txt.
+
+## Input Format
+
+- Input commands should be provided in data/input.txt file, with one command per line.
+- Valid commands:
+  - PLACE X,Y,F
+  - MOVE
+  - LEFT
+  - RIGHT
+  - REPORT
+
+## Output Format
+
+- The simulator writes the output to data/output.txt file.
+- Output format: X,Y,F (X and Y represent the position, F represents the direction).
+
+## Testing
+
+- Unit tests are provided in test/test_toy_robot.py.
+- Run tests using the following command: python3 -m tests.test_toy_robot
+
--- main.py
+++ main.py
@@ -1,38 +1,162 @@
 from modules.placeToyRobot import PlaceToyRobot
 from modules.moveToyRobot import MoveToyRobot
+from modules.directionToyRobot import DirectionToyRobot
+from modules.reportToyRobot import ReportToyRobot
 
 
+def read_input_from_file(file_path):
+    """
+    Reads instructions from a file and returns them as a list of strings.
+    
+    Args:
+        file_path (str): The path to the file containing the instructions.
+    
+    Returns:
+        List[str]: A list of instruction strings.
+    """
+    instructions = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            instructions.append(line.strip())
+        print(instructions)
+    return instructions
+
+def place_robot(x_temp, y_temp, direction_temp):
+    """
+    Attempts to place the robot on the board at the specified coordinates and direction.
+    
+    Args:
+        x_temp (str): The x-coordinate for placing the robot.
+        y_temp (str): The y-coordinate for placing the robot.
+        direction_temp (str): The direction the robot should face.
+    
+    Returns:
+        Tuple[int, int, str]: The coordinates and direction if placement is valid, otherwise (None, None, None).
+    """
+    print(f"Trying to place robot at ({x_temp}, {y_temp}) facing {direction_temp}")
+    objplace = PlaceToyRobot(x_temp, y_temp, direction_temp)
+    if objplace.check_valid_place():
+        print("Robot placed", x_temp, y_temp, direction_temp)
+        return x_temp, y_temp, direction_temp
+    else:
+        return None, None, None
+
+def move_robot(x, y, direction):
+    """
+    Moves the robot one unit forward in the direction it is currently facing.
+    
+    Args:
+        x (int): The current x-coordinate of the robot.
+        y (int): The current y-coordinate of the robot.
+        direction (str): The current direction the robot is facing.
+    
+    Returns:
+        Tuple[int, int]: The new coordinates after moving the robot.
+    """
+    objmove = MoveToyRobot(x, y, direction)
+    return objmove.move_robot()
+
+def turn_left(direction):
+    """
+    Turns the robot 90 degrees to the left.
+    
+    Args:
+        direction (str): The current direction the robot is facing.
+    
+    Returns:
+        str: The new direction after turning left.
+    """
+    objdire = DirectionToyRobot(direction)
+    return objdire.move_left()
+
+def turn_right(direction):
+    """
+    Turns the robot 90 degrees to the right.
+    
+    Args:
+        direction (str): The current direction the robot is facing.
+    
+    Returns:
+        str: The new direction after turning right.
+    """
+    objdire = DirectionToyRobot(direction)
+    return objdire.move_right()
+
+def report_robot(x, y, direction):
+    """
+    Reports the current position and direction of the robot.
+    
+    Args:
+        x (int): The current x-coordinate of the robot.
+        y (int): The current y-coordinate of the robot.
+        direction (str): The current direction the robot is facing.
+    """
+    objreport = ReportToyRobot(x, y, direction)
+    objreport.report()
+
+def execute_instructions(instructions):
+    """
+    Executes a list of instructions to control the robot.
+    
+    Args:
+        instructions (List[str]): A list of instruction strings.
+    """
+    place_flag = False
+    x_final, y_final, direction_final = None, None, None
+    
+    for instruction in instructions:
+        if instruction.upper().startswith('PLACE'):
+            # Split the instruction based on whitespace
+            parts = instruction.split()
+            print("parts",parts)
+            # Check if there are exactly two commas in the instruction
+            if len(parts) == 2 and ',' in parts[1]:
+                # Split the second part based on commas
+                params = parts[1].split(',')
+                print("params",params)
+                
+                # Ensure there are exactly three parameters after splitting
+                if len(params) == 3:
+                    # Assign the parameters to variables
+                    x_temp, y_temp, direction_temp = params
+                    print("Before moving", x_temp, y_temp, direction_temp)
+                    if not (x_temp.isdigit()  and y_temp.isdigit() and isinstance(direction_temp, str)):
+                            continue
+                    
+                    # Check if direction is a valid string
+                    if direction_temp.upper() in ['NORTH', 'SOUTH', 'EAST', 'WEST']:
+                        # Call place_robot function
+                        x_final, y_final, direction_final = place_robot(x_temp, y_temp, direction_temp)
+                        
+                        # Check if placement was successful
+                        if x_final is not None:
+                            place_flag = True
+                            print("Robot placed successfully.")
+                        continue
+                    else:
+                        continue
+                        
+        if place_flag:
+            if instruction.upper() == 'MOVE':
+                x_final, y_final = move_robot(x_final, y_final, direction_final.upper())
+                print("After moving", x_final, y_final, direction_final)
+            elif instruction.upper() == 'LEFT':
+                direction_final = turn_left(direction_final.upper())
+                print("After moving left", x_final, y_final, direction_final)
+            elif instruction.upper() == 'RIGHT':
+                direction_final = turn_right(direction_final.upper())
+                print("After moving right", x_final, y_final, direction_final)
+            elif instruction.upper() == 'REPORT':
+                report_robot(x_final, y_final, direction_final.upper())
+                print("After moving", x_final, y_final, direction_final)
 
 def main():
-    ## Read input from file
-    with open('data/1.txt', 'r') as file:
-        place_flag=0
-        for line in file:
-            # Remove leading/trailing whitespace and newline characters
-            line = line.strip()
-            if line.startswith('PLACE'):
-                _, params = line.split()
-                x_temp, y_temp, direction_temp = params.split(',')
-                print(f"Trying to  place robot at ({x_temp}, {y_temp}) facing {direction_temp}")
-                objplace = PlaceToyRobot(x_temp,y_temp,direction_temp)
-                if(objplace.check_valid_place()):
-                    x_final, y_final, direction_final = x_temp, y_temp, direction_temp
-                    place_flag= 1
-                    print("Robot placed",x_final, y_final, direction_final)
-                continue
+    """
+    The main function that reads instructions from a file and executes them.
+    """
+    instructions = read_input_from_file('data/5.txt')
+    execute_instructions(instructions)
 
-            if(place_flag==1):
-                if line.startswith('MOVE'):
-                    objmove=MoveToyRobot(x_final,y_final,direction_final)
-                    x_final,y_final=objmove.move_robot()
-                    print("After moving",x_final,y_final,direction_final)
-
-
-                
-
-
-
-            # Add more conditions for other commands as needed
 
 if __name__ == "__main__":
     main()
